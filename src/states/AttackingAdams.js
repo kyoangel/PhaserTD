69fd6f87e62aacb97ce16e3b2fa5f3e21128369d
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-08-22 18:20:42 +0800

    could check game over

--- src/states/AttackingAdams.js
+++ src/states/AttackingAdams.js
@@ -1,35 +1,44 @@
+/// <reference path="./../defs/phaser.d.ts"/>
 var AttackingAdams = function () { };
 AttackingAdams.prototype = {
+    life: 3,
     preload: function () {
         this.load.image('base', './assets/sprites/cannonBase.png');
         this.load.image('barrel', './assets/sprites/cannonBarrel.png');
         this.load.image('bullet', './assets/sprites/bullet.png');
         this.load.image('adam', './assets/sprites/adam.png');
+        this.load.image('wall', './assets/sprites/wall.png');
     },
     create: function () {
         this.score = 0;
+        this.enemySpeedUp = 0;
+        this.walls = this.physics.add.group();
+        this.walls.allowGravity = false;
+        this.buildWall();
+
         this.text = this.add.text(0, 0, "Your Score: 0", { font: "40px Impact" });
 
         this.base = this.add.sprite(100, 300, 'base').setOrigin(0.5, 0.5);
         this.base.setScale(0.4);
 
         this.bullet = this.physics.add.sprite(100, 300, 'bullet').setOrigin(0, 0.5);
-
         this.bullet.setScale(0.85, 0.85);
         this.bullet.body.allowGravity = false;
 
         this.barrel = this.add.sprite(100, 300, 'barrel').setOrigin(0.3, 0.5);
         this.barrel.setScale(0.5);
 
-        this.enemy1 = this.physics.add.sprite(600, 100, 'adam');
-        this.enemy1.body.allowGravity = false;
-        this.enemy1.setScale(0.3);
-        this.enemy2 = this.physics.add.sprite(600, 300, 'adam');
-        this.enemy2.body.allowGravity = false;
-        this.enemy2.setScale(0.3);
-        this.enemy3 = this.physics.add.sprite(600, 500, 'adam');
-        this.enemy3.body.allowGravity = false;
-        this.enemy3.setScale(0.3);
+        this.enemies = [];
+        var y = 100;
+        for (var i = 0; i < 3; i++) {
+            var enemy = this.physics.add.sprite(800, y, 'adam');
+            enemy.body.allowGravity = false;
+            enemy.setScale(0.3);
+            enemy.flipX = true;
+            this.enemies.push(enemy);
+            this.enemyAttack(enemy);
+            y += 200;
+        }
 
         this.input.on('pointermove', function (pointer) {
             let cursor = pointer;
@@ -51,29 +60,73 @@ AttackingAdams.prototype = {
 
     },
     update: function () {
-        this.physics.world.overlap(this.enemy1, this.bullet, this.hitEnemy, null, this);
-        this.physics.world.overlap(this.enemy2, this.bullet, this.hitEnemy, null, this);
-        this.physics.world.overlap(this.enemy3, this.bullet, this.hitEnemy, null, this);
+        var self = this;
+        this.enemies.forEach(enemy => {
+            if (!enemy.isFirst) {
+                this.physics.world.overlap(enemy, this.bullet, this.hitEnemy, null, self);
+                this.physics.world.overlap(enemy, this.walls, this.breakWall, null, self);
+            }
+            else {
+                enemy.isFirst = false;
+            }
+        });
+
         if (this.bullet.x > window.innerWidth || this.bullet.y > window.innerHeight
             || this.bullet.x < 0 || this.bullet.y < 0) {
-            this.reset();
+            this.reset(null);
         }
     },
     fire: function (x, y) {
         this.physics.moveTo(this.bullet, x, y, 60, 750);
-    }
-    ,
-    reset: function () {
+    },
+    enemyAttack: function (enemy) {
+        this.physics.moveTo(enemy, this.base.x, enemy.y, 60 + this.enemySpeedUp, 0);
+    },
+    reset: function (enemy) {
         this.bullet.x = 100;
         this.bullet.y = 300;
         this.bullet.setVelocity(0, 0);
+        if (enemy) {
+            enemy.x = 900;
+            this.enemySpeedUp += Math.floor((Math.random() * 10));
+        }
     },
-    hitEnemy: function (e) {
-        this.reset();
-        e.destroy();
+    hitEnemy: function (enemy, bullet) {
+        this.reset(enemy);
         this.score += 100;
         this.text.setText("Your Score: " + this.score);
+    },
+    buildWall: function () {
+        for (var i = 0; i < 650; i += 36) {
+            var wall = this.physics.add.sprite(140, i, 'wall').setOrigin(0, 0);
+            wall.body.allowGravity = false;
+            wall.setScale(0.1);
+            this.walls.add(wall);
+        }
+    },
+    breakWall: function (enemy, wall) {
+        var y = enemy.y;
+        enemy.destroy();
+        for (var i=0;i<3;i++){
+            var oldEnemy = this.enemies[i];
+            if (y == oldEnemy.y) {
+                var newEnemy = this.physics.add.sprite(800, y, 'adam');
+                newEnemy.body.allowGravity = false;
+                newEnemy.setScale(0.3);
+                newEnemy.flipX = true;
+                newEnemy.isFirst = true;
+                newEnemy.setVelocity(0);
+                this.enemies[i]= newEnemy;
+                this.enemyAttack(newEnemy);
+                break;
+            }
+        }
+
+        this.life--;
+        if (this.life <= 0)
+           alert("Game Over!");
     }
+
 
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    devtool: false,
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({})
+    ]
 };
